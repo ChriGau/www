cfe380a978aa21af1da5b6c52860ee180dcf2073
cedricmenteau <me.cedric@gmail.com>
2016-12-29 15:22:57 +0100

    Fix review counter

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,10 +2,11 @@ class StudentsController < ApplicationController
   respond_to :html, :js
 
   def index
-    @testimonials = @client.testimonials(locale.to_s)
+    @testimonials = Kaminari.paginate_array(@client.testimonials(locale.to_s)).page(params[:page]).per(6)
     @projects = @client.projects("alumni_projects")
     @statistics = @client.statistics
     @stories = @client.stories
     @stories =  Kaminari.paginate_array(@stories).page(params[:page]).per(4)
+    @reviews = ReviewsCounter.new.review_count
   end
 end
