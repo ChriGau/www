330b1c03f3a6cb328ec13bbf7a6b4f7597f4e301
cedricmenteau <me.cedric@gmail.com>
2016-12-30 12:04:00 +0100

    fix testimonials ajax

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,12 +2,20 @@ class StudentsController < ApplicationController
   respond_to :html, :js
 
   def index
-    @testimonials = @client.testimonials(locale.to_s)
-    @testimonials = Kaminari.paginate_array(@testimonials).page(params[:page]).per(6)
-    @projects = @client.projects("alumni_projects")
-    @statistics = @client.statistics
-    @stories = @client.stories
-    @stories =  Kaminari.paginate_array(@stories).page(params[:page]).per(4)
-    @reviews = ReviewsCounter.new.review_count
+    if request.format.html? || params[:testimonial_page]
+      @testimonials = @client.testimonials(locale.to_s)
+      @testimonials = Kaminari.paginate_array(@testimonials).page(params[:testimonial_page]).per(6)
+    end
+
+    if request.format.html? || params[:story_page]
+      @stories = @client.stories
+      @stories =  Kaminari.paginate_array(@stories).page(params[:story_page]).per(4)
+    end
+
+    if request.format.html?
+      @projects = @client.projects("alumni_projects")
+      @statistics = @client.statistics
+      @reviews = ReviewsCounter.new.review_count
+    end
   end
 end
